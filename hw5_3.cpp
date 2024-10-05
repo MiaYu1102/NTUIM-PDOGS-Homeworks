7ed1161191887ecee1ed79cf24cab647032b496e
MiaYu1102 <mia.c.e.yu@gmail.com>
2024-10-05 11:49:22 +0800

    Update hw5_3.cpp

--- hw5_3.cpp
+++ hw5_3.cpp
@@ -1,10 +1,8 @@
 #include <climits>
 #include <cmath>
-#include <algorithm>
 #include <iostream>
 using namespace std;
 
-//剛寫好拿上去丟的版本
 
 const int MAX_Elevator_CNT = 10;
 
@@ -18,33 +16,37 @@ int getMin(int a, int b);
 int getMax(int a, int b);
 
 int countSum(const int arr[], int n);
+void swap(int &a, int &b);
+void bubbleSort(int* arr, int n);
 
-int main() {
-    // 初
-    int n, k, timePerFloor, timePerDoorCycle;
-    cin >> n >> k >> timePerFloor >> timePerDoorCycle;
 
-    // 本體
+int main() 
+{
+    // Initialize variables for the number of elevators, floors, and time intervals
+    int elevatorCnt, floorCnt, timePerFloor, timePerDoorCycle;
+    cin >> elevatorCnt >> floorCnt >> timePerFloor >> timePerDoorCycle;
+
+    // Arrays to store the current floor and direction of each elevator
     int ElevatorFloors[MAX_Elevator_CNT];
     int ElevatorDirections[MAX_Elevator_CNT];
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < elevatorCnt; i++) {
         cin >> ElevatorFloors[i] >> ElevatorDirections[i];
     }
 
-    //
+    // Arrays to track the highest and lowest floors each elevator will visit
     int highestFloor[MAX_Elevator_CNT];
     int lowestFloor[MAX_Elevator_CNT];
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < elevatorCnt; i++) {
         highestFloor[i] = 1;
-        lowestFloor[i]  = k;
+        lowestFloor[i]  = floorCnt;
     }
 
-    // 內
+    // Input the inner calls (calls made from inside the elevator)
     int** innerCall = nullptr;
     innerCall       = new int*[MAX_Elevator_CNT];
     int innCallCnt[MAX_Elevator_CNT];
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < elevatorCnt; i++) {
         cin >> innCallCnt[i];
         innerCall[i] = new int[innCallCnt[i] + 1];
         for (int j = 0; j < innCallCnt[i]; j++) {
@@ -54,14 +56,14 @@ int main() {
         }
     }
 
-    // 外
+    // Input the outer calls (calls made from outside the elevator)
     int** outerCall           = nullptr;
-    int** outerCallDirections = nullptr;  //
+    int** outerCallDirections = nullptr;  
     outerCall                 = new int*[MAX_Elevator_CNT];
     outerCallDirections       = new int*[MAX_Elevator_CNT];
 
     int outCallCnt[MAX_Elevator_CNT];
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < elevatorCnt; i++) {
         cin >> outCallCnt[i];
         outerCall[i]           = new int[outCallCnt[i] + 1];
         outerCallDirections[i] = new int[outCallCnt[i] + 1];
@@ -73,13 +75,13 @@ int main() {
         }
     }
 
-    // 待
-    int f, d;
-    cin >> f >> d;
+    // Input the floor and direction of the new request
+    int newCallFloor, newCallDirection;
+    cin >> newCallFloor >> newCallDirection;
 
-    // 原時間計算
+    // Calculate the initial waiting time for each elevator
     int timeRec[MAX_Elevator_CNT];
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < elevatorCnt; i++) {
         if (ElevatorDirections[i] == 0) {
             int time_up, time_down;
             time_up    = getWaitingTime(ElevatorFloors[i], 1,
@@ -103,19 +105,20 @@ int main() {
 										timePerFloor, timePerDoorCycle);
     }
 
-    int sum = countSum(timeRec, n);
+    int sum = countSum(timeRec, elevatorCnt);
 
-    // 暴力嘗試每台電梯
+    // Try to find the best elevator for the new request
     int bestElevatorindex = -1;
     int minimumTime       = INT_MAX;
-    for (int i = 0; i < n; i++) {
-        // 把待接塞進原陣列
-        outerCall[i][outCallCnt[i]]  = f;
-        outerCallDirections[i][outCallCnt[i]] = d;
+    for (int i = 0; i < elevatorCnt; i++) 
+    {
+        // Add the new request to the current outer calls of the elevator
+        outerCall[i][outCallCnt[i]]  = newCallFloor;
+        outerCallDirections[i][outCallCnt[i]] = newCallDirection;
         highestFloor[i]           = getMax(highestFloor[i], outerCall[i][outCallCnt[i]]);
         lowestFloor[i]            = getMin(lowestFloor[i], outerCall[i][outCallCnt[i]]);
 
-        int t;
+        int newCallTime;
         if (ElevatorDirections[i] == 0) {
             int time_up, time_down;
             time_up   = getWaitingTime(ElevatorFloors[i], 1, 
@@ -130,17 +133,17 @@ int main() {
 									   innerCall[i], outerCall[i], outerCallDirections[i],
 									   timePerFloor, timePerDoorCycle);
 
-            t = getMin(time_up, time_down);
+            newCallTime = getMin(time_up, time_down);
 
         } else {
-            t = getWaitingTime(ElevatorFloors[i], ElevatorDirections[i],
+            newCallTime = getWaitingTime(ElevatorFloors[i], ElevatorDirections[i],
                                highestFloor[i], lowestFloor[i], 
 							   innCallCnt[i], outCallCnt[i] + 1, 
 							   innerCall[i], outerCall[i], outerCallDirections[i],
 							   timePerFloor, timePerDoorCycle);
 		}
 
-        int currentTime = sum - timeRec[i] + t;
+        int currentTime = sum - timeRec[i] + newCallTime;
         if (currentTime < minimumTime) {
             minimumTime       = currentTime;
             bestElevatorindex = i;
@@ -151,13 +154,13 @@ int main() {
     cout << ++bestElevatorindex << "," << minimumTime << endl;
 
     // release the dynamically allocated memory
-    for (int i = 0; i < n; i++) delete[] innerCall[i];
+    for (int i = 0; i < elevatorCnt; i++) delete[] innerCall[i];
     delete[] innerCall;
 
-    for (int i = 0; i < n; i++) delete[] outerCall[i];
+    for (int i = 0; i < elevatorCnt; i++) delete[] outerCall[i];
     delete[] outerCall;
 
-    for (int i = 0; i < n; i++) delete[] outerCallDirections[i];
+    for (int i = 0; i < elevatorCnt; i++) delete[] outerCallDirections[i];
     delete[] outerCallDirections;
 
     return 0;
@@ -182,25 +185,25 @@ int getWaitingTime(int currentFloor, int currentDirection,
     } else if (currentFloor < lowestFloor && currentDirection == -1) {
         currentDirection *= -1;
     }
-    // 外
+
+    // Calculate waiting time for outer calls
     if (currentDirection == 1) {
         for (int i = 0; i < outerCallCnt; i++) {
 
             if (currentDirection == outerCallDirections[i]) {
 
                 if (outerCallFloors[i] > currentFloor) {
-                    WaitingTime[i]= (outerCallFloors[i] - currentFloor);
+                    WaitingTime[i] = (outerCallFloors[i] - currentFloor);
                 } else if (outerCallFloors[i] == currentFloor) {
-                    WaitingTime[i]= 0;
+                    WaitingTime[i] = 0;
                 } else {
-                    WaitingTime[i]= 2 * (highestFloor - lowestFloor) -
+                    WaitingTime[i] = 2 * (highestFloor - lowestFloor) -
                                     (currentFloor - outerCallFloors[i]);
                 }
 
             } else {
                 WaitingTime[i]= (highestFloor - currentFloor) +
                                 (highestFloor - outerCallFloors[i]);
-              
             }
         }
     }
@@ -208,28 +211,28 @@ int getWaitingTime(int currentFloor, int currentDirection,
         for (int i = 0; i < outerCallCnt; i++) {
             if (currentDirection == outerCallDirections[i]) {
                 if (outerCallFloors[i] < currentFloor) {
-                    WaitingTime[i]= (currentFloor - outerCallFloors[i]);
+                    WaitingTime[i] = (currentFloor - outerCallFloors[i]);
                 } else if (outerCallFloors[i] == currentFloor) {
-                    WaitingTime[i]= 0;
+                    WaitingTime[i] = 0;
                 } else {
-                    WaitingTime[i]= 2 * (highestFloor - lowestFloor) -
-                                    (outerCallFloors[i] - currentFloor);
+                    WaitingTime[i] = 2 * (highestFloor - lowestFloor) -
+                                     (outerCallFloors[i] - currentFloor);
                 }
 
             } else {
-                WaitingTime[i]= (currentFloor - lowestFloor) +
+                WaitingTime[i] = (currentFloor - lowestFloor) +
                                 (outerCallFloors[i] - lowestFloor);
             }
         }
     }
 
-    // 內
+    // Calculate waiting time for inner calls
     if (currentDirection == 1) {
         for (int i = 0; i < innerCallCnt; i++) {
             if (innerCallFloors[i] >= currentFloor) {
-                WaitingTime[i+outerCallCnt]= (innerCallFloors[i] - currentFloor);
+                WaitingTime[i + outerCallCnt] = (innerCallFloors[i] - currentFloor);
             } else {
-                WaitingTime[i+outerCallCnt]= (highestFloor - currentFloor) +
+                WaitingTime[i + outerCallCnt] = (highestFloor - currentFloor) +
                                 (highestFloor - innerCallFloors[i]);
             }
         }
@@ -237,28 +240,28 @@ int getWaitingTime(int currentFloor, int currentDirection,
     if (currentDirection == -1) {
         for (int i = 0; i < innerCallCnt; i++) {
             if (innerCallFloors[i] <= currentFloor) {
-                WaitingTime[i+outerCallCnt]= (currentFloor - innerCallFloors[i]);
+                WaitingTime[i + outerCallCnt] = (currentFloor - innerCallFloors[i]);
             } else {
-                WaitingTime[i+outerCallCnt]= (currentFloor - lowestFloor) +
+                WaitingTime[i + outerCallCnt] = (currentFloor - lowestFloor) +
                                 (innerCallFloors[i] - lowestFloor);
             }
         }
     }
-    sort(WaitingTime,WaitingTime+(innerCallCnt + outerCallCnt));
-    int doorCnt=0;
-    int temp=0;
-    for(int i=0;i<(innerCallCnt + outerCallCnt);i++){
-        doorCnt+=temp;
-        temp++;
-        if(i>0 && WaitingTime[i-1]==WaitingTime[i]){
-            temp--;
+    bubbleSort(WaitingTime, innerCallCnt + outerCallCnt);
+    int doorCnt = 0;
+    int tempCnt = 0;
+    for(int i = 0; i < (innerCallCnt + outerCallCnt); i++){
+        doorCnt += tempCnt;
+        tempCnt++;
+        if(i > 0 && WaitingTime[i-1] == WaitingTime[i]){
+            tempCnt--;
             doorCnt--;
         } 
     }
 
-    int WaitingTimes=0;
-    for(int i=0;i<(innerCallCnt + outerCallCnt);i++){
-        WaitingTimes+=WaitingTime[i];
+    int WaitingTimes = 0;
+    for(int i = 0; i < (innerCallCnt + outerCallCnt); i++){
+        WaitingTimes += WaitingTime[i];
     }
 
     return WaitingTimes * timePerFloor + timePerDoorCycle * doorCnt;
@@ -284,4 +287,24 @@ int countSum(const int arr[], int n) {
         sum += arr[i];
     }
     return sum;
+}
+
+void swap(int &a, int &b) {
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+void bubbleSort(int* arr, int n) {
+	for (int i = 0; i < n - 1; i++) {
+        bool swapped = 0;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = 1;
+            }
+        }
+        if (!swapped)
+            break;
+    }	
 }
