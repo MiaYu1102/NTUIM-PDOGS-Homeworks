2af798576fabb8bd81278988fc65566a6c52781d
MiaYu1102 <mia.c.e.yu@gmail.com>
2024-11-10 20:44:35 +0800

    Update hw8

--- Homework 8/hw8-2.cpp
+++ Homework 8/hw8-2.cpp
@@ -1,10 +1,193 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <fstream>
 using namespace std;
 
+class Patient
+{
+private:
+    int age; // 病人的年齡
+    int arrivalTime; // 病人到達診所的時間
+    int serviceTime; // 病人看診所需的時間
+    int finishTime; // 病人看診結束並離開診所的時間
+    int waitingTime; // 病人從到達診所到開始看診的等待時間
+    int patientsInClinicCnt; // 病人到達診所時在診所裡的病人數量
+public:
 
+    // 建構子
+    // a: 到達時間; s: 看診時間
+    Patient(int g, int a, int s);
 
-int main(){
+    // 計算看診結束時間和等待時間
+    // currTime: 當前時間; startServiceTime: 病人開始看診的時間
+    void computeFinishAndWaitingTime(int startServiceTime);
+
+    // 如果病人和另一位病人年齡差異大於等於設定的閾值，返回 true
+    bool hasHigherPrior(Patient* another, int threshold);
     
+    // 返回病人是否已經離開診所
+    bool isLeft(int currTime);
+
+    // 設定在診所內的病人數量
+    void setTotalPatients(int currPatientIdx, int nextLeftPatientIdx);
+
+    // 輸出指定病人的資訊
+    void printInformation() { cout << finishTime << ',' << waitingTime << ',' << patientsInClinicCnt << ','; };
+
+    // 取得相關資料
+    int getArrivalTime() { return arrivalTime; };
+    int getServiceTime() { return serviceTime; };
+    int getFinishTime() { return finishTime; };
+    int getWaitingTime() { return waitingTime; };
+    int getPatientsInClinicCnt() { return patientsInClinicCnt; };
+};
+
+Patient::Patient(int g, int a, int s)
+{
+    // 初始化
+    age = g;
+    arrivalTime = a;
+    serviceTime = s;
+    finishTime = 0;
+    waitingTime = 0;
+    patientsInClinicCnt = 0;
+}
+
+bool Patient::hasHigherPrior(Patient* another, int threshold)
+{
+    if (this->age >= another->age + threshold)
+        return true;
+    else
+        return false;
+}
+
+void Patient::computeFinishAndWaitingTime(int startServiceTime)
+{
+    finishTime = startServiceTime + serviceTime;
+    waitingTime = startServiceTime - arrivalTime;
+}
+
+bool Patient::isLeft(int currTime)
+{
+    if (currTime > arrivalTime && currTime <= finishTime)
+        return false;
+    else
+        return true;
+}
+
+void Patient::setTotalPatients(int currPatientIdx, int nextLeftPatientIdx)
+{
+    patientsInClinicCnt = currPatientIdx - nextLeftPatientIdx;
+}
+
+void swapServiceOrder(Patient& a, Patient& b)
+{
+    Patient temp = a;
+    a = b;
+    b = temp;
+}
+
+void swapIdx(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main()
+{
+    // 讀取資料
+    int totalPatientCnt = 0, ageThreshold = 0;
+    cin >> totalPatientCnt >> ageThreshold;
+    int* ages = new int[totalPatientCnt];
+    for (int i = 0; i < totalPatientCnt; i++)
+        cin >> ages[i];
+    int* arrivalTimeList = new int[totalPatientCnt];
+    for (int i = 0; i < totalPatientCnt; i++)
+        cin >> arrivalTimeList[i];
+    int* serviceTimeList = new int[totalPatientCnt];
+    for (int i = 0; i < totalPatientCnt; i++)
+        cin >> serviceTimeList[i];
+    int kthPatient = 0, givenTime = 0;
+    cin >> kthPatient;
+    cin.ignore(); // 忽略逗號
+    cin >> givenTime;
+
+    // 創建變數
+    Patient** serviceOrder = new Patient*[totalPatientCnt]; // 病人服務順序
+    int* serviceOrderIdx = new int [totalPatientCnt]; // 儲存病人到達的順序
+    for (int i = 0; i < totalPatientCnt; i++)
+    {
+        serviceOrder[i] = new Patient(ages[i], arrivalTimeList[i], serviceTimeList[i]); // 創建物件並初始化
+        serviceOrderIdx[i] = i + 1;
+    }
+
+    int onServiceTime = serviceOrder[0]->getArrivalTime(); // 醫生結束看診並可為下一位病人服務的時間
+    int startServiceTime = serviceOrder[0]->getArrivalTime(); // 病人開始看診的時間
+    int nextLeftPatientIdx = 0; // 下一位將離開診所的病人索引
+    int nextComePatientIdx = 1; // 下一位將到達診所的病人索引
+
+    /* 
+    此 while 迴圈邏輯：
+    - 使用兩個索引追蹤下一位將離開和到達的病人
+    - 計算病人離開時的等待時間和結束時間，並更新醫生的服務時間
+    - 在醫生服務期間，如果有病人到達，內部 while 迴圈會檢查是否需要調整服務順序。如果需要，則交換順序
+    - 計算病人到達時診所內的病人數量，即每當病人到達時，將 nextComePatientIdx 加 1
+    */
+    while (nextLeftPatientIdx < totalPatientCnt)
+    {
+        onServiceTime += serviceOrder[nextLeftPatientIdx]->getServiceTime(); // 更新醫生可為下一位病人服務的時間
+
+        while (nextComePatientIdx < totalPatientCnt && serviceOrder[nextComePatientIdx]->getArrivalTime() <= onServiceTime) // 如果有病人到達且醫生正在服務其他病人
+        {
+            // 計算診所內的病人數量
+            serviceOrder[nextComePatientIdx]->setTotalPatients(nextComePatientIdx, nextLeftPatientIdx);
+
+            // 調整目前等候病人的服務順序
+            int adjustIdx = 0;
+            for (int i = nextComePatientIdx - 1; i > nextLeftPatientIdx; i--)
+            {
+                if (serviceOrder[nextComePatientIdx - adjustIdx]->hasHigherPrior(serviceOrder[i], ageThreshold)) // 檢查優先權
+                {
+                    // 後來的病人有更高的優先權 -> 改變順序（交換）
+                    swapServiceOrder(*serviceOrder[i], *serviceOrder[nextComePatientIdx - adjustIdx]);
+                    swapIdx(serviceOrderIdx[i], serviceOrderIdx[nextComePatientIdx - adjustIdx]);
+                    adjustIdx++;
+                }
+                else // 一旦條件不符合，停止進一步比較
+                    break;
+            }
+
+            // 跳至下一位病人
+            nextComePatientIdx++;
+        }
+        
+        // 計算病人的等待時間和結束時間
+        serviceOrder[nextLeftPatientIdx]->computeFinishAndWaitingTime(startServiceTime);
+
+        // 如果沒有病人在等待，看診時間更新
+        if (nextLeftPatientIdx < totalPatientCnt - 1 && serviceOrder[nextLeftPatientIdx + 1]->getArrivalTime() >= onServiceTime)
+            onServiceTime = serviceOrder[nextLeftPatientIdx + 1]->getArrivalTime();
+        
+        // 醫生移至下一位病人
+        nextLeftPatientIdx++;
+        startServiceTime = onServiceTime;
+    }
+
+    // 計算在指定時間內診所內的病人數量並查找指定病人在重新排序後的索引
+    int patientsCntT = 0;
+    int WTFPatientIdx = 0;
+    for (int i = 0; i < totalPatientCnt; i++)
+    {
+        if (!serviceOrder[i]->isLeft(givenTime)) // 如果病人尚未離開，即仍在診所
+            patientsCntT++;
+
+        if (serviceOrderIdx[i] == kthPatient) // 通過查找到達索引來找到指定病人
+            WTFPatientIdx = i;
+    }
+
+    // 輸出結果
+    serviceOrder[WTFPatientIdx]->printInformation();
+    cout << patientsCntT;
 
     return 0;
 }
--- Homework 8/hw8-3.cpp
+++ Homework 8/hw8-3.cpp
@@ -5,8 +5,7 @@ using namespace std;
 int MAX_SIZE = 5000;
 int MAX_MATRIX_CNT = 25;
 
-class Matrix
-{
+class Matrix {
 private: 
     int n;
     int m;
@@ -16,21 +15,23 @@ public:
     char name;
     Matrix();
     Matrix(int n, int m);
+    Matrix(const Matrix& mat);
     ~Matrix();
 
     friend ostream& operator << (ostream& out, const Matrix& mat);
     friend istream& operator >> (istream& in, Matrix& mat);
+    Matrix& operator = (const Matrix& mat);
     Matrix operator + (const Matrix& mat);
     Matrix operator - (const Matrix& mat);
     Matrix operator * (const Matrix& mat);
-    Matrix operator ! ();
     Matrix operator * (int num);
+    Matrix operator ! ();
     bool isEmpty() const;
 
 };
 
 // constructor
-Matrix::Matrix() : n(0), m(0), data(NULL) {}
+Matrix::Matrix() : n(0), m(0), data(nullptr) {}
 Matrix::Matrix(int n, int m) : n(n), m(m) {
     this->data = new int*[n];
     for (int i = 0; i < n; i++) {
@@ -40,15 +41,57 @@ Matrix::Matrix(int n, int m) : n(n), m(m) {
 
 // destructor
 Matrix::~Matrix() {
-    for (int i = 0; i < n; i++) {
-        delete[] data[i];
+    if (data) {
+        for (int i = 0; i < n; i++) {
+            if(data[i]) delete[] data[i];
+        }
+        delete[] data;
     }
-    delete[] data;
 }
 
-//加
+// copy constructor
+Matrix::Matrix(const Matrix& mat) {
+    n = mat.n;
+    m = mat.m;
+    data = new int*[n];
+    for (int i = 0; i < n; i++) {
+        data[i] = new int[m];
+        for (int j = 0; j < m; j++) {
+            data[i][j] = mat.data[i][j];
+        }
+    }
+}
+
+// copy assignment
+Matrix& Matrix::operator = (const Matrix& mat) {
+    if (this == &mat) return *this;
+
+    // 釋放原本的記憶體
+    if (data) {
+        for (int i = 0; i < n; i++) {
+            if(data[i]) delete[] data[i];
+        }
+        delete[] data;
+    }
+
+    // 複製新的記憶體
+    n = mat.n;
+    m = mat.m;
+    data = new int*[n];
+    for (int i = 0; i < n; i++) {
+        data[i] = new int[m];
+        for (int j = 0; j < m; j++) {
+            data[i][j] = mat.data[i][j];
+        }
+    }
+
+    return *this;
+}
+
+// 加
 Matrix Matrix::operator + (const Matrix& mat) {
     if (n != mat.n || m != mat.m) {
+        cerr<<"ERROR 3"<<endl;
         return Matrix();
     }
     Matrix result(n, m);
@@ -60,9 +103,10 @@ Matrix Matrix::operator + (const Matrix& mat) {
     return result;
 }
 
-//減
+// 減
 Matrix Matrix::operator - (const Matrix& mat) {
     if (n != mat.n || m != mat.m) {
+        cerr<<"ERROR 2"<<endl;
         return Matrix();
     }
     Matrix result(n, m);
@@ -74,9 +118,10 @@ Matrix Matrix::operator - (const Matrix& mat) {
     return result;
 }
 
-//與矩陣相乘
+// 與矩陣相乘
 Matrix Matrix::operator * (const Matrix& mat) {
     if (m != mat.n) {
+        cerr<<"ERROR 1"<<endl;
         return Matrix();
     }
     
@@ -104,8 +149,7 @@ Matrix Matrix::operator * (int num) {
     return result;
 }
 
-
-//轉置
+// 轉置
 Matrix Matrix::operator ! () {
     Matrix result(m, n);  // 新矩陣的行數為 m，列數為 n
 
@@ -118,66 +162,106 @@ Matrix Matrix::operator ! () {
     return result;
 }
 
-//判斷為空
+// 判斷為空
 bool Matrix::isEmpty() const {
     return (n == 0 || m == 0 || data == nullptr);
 }
 
-int main()
-{
-    //輸入運算式
-    char expression[MAX_SIZE];
-    cin>>expression;
+int main() {
 
-    //讀取運算式內的矩陣
+    // 輸入運算式
+    char expression[MAX_SIZE];
+    cin >> expression;
+
+    // 讀取運算式內的矩陣
     char matrix_list[MAX_MATRIX_CNT];
+    int matrix_index[256] = {0};
     int matrix_cnt = 0;
     int len = strlen(expression);
 
-    for(int k=0;k<len;k++)
-    {
-        if(isdigit(expression[k]) && strchr(matrix_list, expression[k]) == nullptr) //是字母&&矩陣尚未被建立
-        {
+    for (int k = 0; k < len; k++) {
+        if (isalpha(expression[k]) && strchr(matrix_list, expression[k]) == nullptr) { // 是字母 && 矩陣尚未被建立
             matrix_list[matrix_cnt] = expression[k];
+            matrix_index[expression[k]] = matrix_cnt;
             matrix_cnt++;
         }
     }
 
-    //輸入矩陣
+    // 輸入矩陣
     Matrix mat[matrix_cnt];
-    for(int i=0;i<matrix_cnt;i++){
-        cin >> mat[matrix_cnt];
+    for (int i = 0; i < matrix_cnt; i++) {
+        cin >> mat[i];
     }
 
-    //輸入向量
+    // 輸入向量
     Matrix vec;
     cin >> vec;
 
-    //處理運算式(手刻stack)
     
-    //先處理*和!
-    
-    //再處理+和-
-    Matrix exp_res;
+    // 處理運算式
+    // 先處理*和!
+    int product_cnt = 0;
+    Matrix product_terms[len];
+    int multiply_const = 1;
 
-    //計算向量乘法
+    for (int i = 0; i < len; i++) {
+        if (isdigit(expression[i])) {
+            multiply_const *= expression[i] - '0';
+        }
+        else if (expression[i] == '+') {
+            product_terms[product_cnt] = product_terms[product_cnt] * multiply_const;
+            product_cnt++;
+            multiply_const = 1;
+        } 
+        else if (expression[i] == '-') {
+            product_terms[product_cnt] = product_terms[product_cnt] * multiply_const;
+            product_cnt++;
+            multiply_const = -1;
+        }
+        else if (expression[i] == '!') {
+            continue;
+        }
+        else {
+            Matrix temp = mat[matrix_index[expression[i]]];
+
+            if (i != len - 1 && expression[i + 1] == '!') {
+                temp = !mat[matrix_index[expression[i]]];
+            }
+            
+            if (product_terms[product_cnt].isEmpty()) {
+                product_terms[product_cnt] = temp;
+            } else {
+                product_terms[product_cnt] = product_terms[product_cnt] * temp;
+            }
+        }
+    }
+    
+    product_terms[product_cnt] = product_terms[product_cnt] * multiply_const;
+
+    // 加起來
+    Matrix exp_res = product_terms[0];
+    for (int i = 1; i <= product_cnt; i++) {
+        exp_res = exp_res + product_terms[i];
+    }
+
+    // 計算向量乘法
     Matrix result = exp_res*vec;
-
-    //輸出結果
-    if(result.isEmpty()) cout<<-1;
-    else cout<<result;
-
+    
+    // 輸出結果
+    if (result.isEmpty()) cout << -1<<endl;
+    else cout << result;
+    
     return 0;
 }
 
 
 //輸出矩陣
-ostream& operator<<(ostream& out, const Matrix& mat)
-{
-    for (int i = 0; i < mat.m; i++) {
-        for (int j = 0; j < mat.n; j++) {
+ostream& operator<<(ostream& out, const Matrix& mat) {
+    for (int i = 0; i < mat.n; i++) {
+        for (int j = 0; j < mat.m-1; j++) {
             out << mat.data[i][j] << " ";
         }
+        out << mat.data[i][mat.m-1];
         out << "\n";
     }
 
@@ -188,10 +272,12 @@ ostream& operator<<(ostream& out, const Matrix& mat)
 istream& operator >> (istream& in, Matrix& mat) {
 
     // 釋放原本的記憶體
-    for (int i = 0; i < mat.n; i++) {
-            delete[] mat.data[i];
+    if (mat.data) {
+        for (int i = 0; i < mat.n; i++) {
+            if(mat.data[i]) delete[] mat.data[i];
+        }
+        delete[] mat.data;
     }
-    delete[] mat.data;
 
     in >> mat.name >> mat.n >> mat.m;
 
@@ -210,4 +296,3 @@ istream& operator >> (istream& in, Matrix& mat) {
   
     return in;
 }
-
