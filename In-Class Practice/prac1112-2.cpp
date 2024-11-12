1475de4d4216843e2557ed1bb57bf452668d40dc
MiaYu1102 <mia.c.e.yu@gmail.com>
2024-11-12 15:43:01 +0800

    Add homework instructions

--- In-Class Practice/prac1112-2.cpp
+++ In-Class Practice/prac1112-2.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main(){
     int n; cin >> n;
     string x; cin >> x;
+    transform(x.begin(), x.end(), x.begin(), ::tolower);
     
     for(int i=0; i<n; i++){
         string d; cin >> d;
@@ -12,6 +13,7 @@ int main(){
             return 0;
         }
     }
+
     cout << 0;
     return 0;
 }
