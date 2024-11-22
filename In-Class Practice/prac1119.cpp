468b00b419ba36b023ed317ce8c69ab9e28db7fe
MiaYu1102 <mia.c.e.yu@gmail.com>
2024-11-22 10:54:49 +0800

    Update prac1119.cpp

--- In-Class Practice/prac1119.cpp
+++ In-Class Practice/prac1119.cpp
@@ -31,17 +31,110 @@ class Car : public Entity
 private:
 public:
   Car(string id, bool isOn, bool isSer, double lon, double lat);
+  void print();
 };
 
 Car::Car(string id, bool isOn, bool isSer, double lon, double lat)
 : Entity(id, isOn, isSer, lon, lat)
 {
 }
+void Car::print()
+{
+    cout << this->id << ": ";
+    if(this->isOn){
+        if(this->isSer){
+            cout<<"in-service, (";
+        }
+        else{
+            cout<<"empty, (";
+        }
+        cout << this->lon << "," << this->lat <<")";
+    }
+    else
+        cout<<"offline";
+    cout<<endl;
+}
 
+class CarArray
+{
+private:
+    int capacity; // initialize it to 20000; may change later
+    int cnt;
+    Car** carPtr;
+    public:
+    CarArray();
+    // CarArray(const CarArray& ca); // not needed
+    // operator=(const CarArray& ea); // in this problem
+    ~CarArray();
+    bool add(string id, bool isOn, bool isSer, double lon, double lat);
+    void print();
+};
+
+CarArray::CarArray()
+{
+    this->cnt = 0;
+    this->capacity = 20000;
+    this->carPtr = new Car*[this->capacity];
+}
+CarArray::~CarArray()
+{
+    for(int i = 0; i < this->cnt; i++)
+        delete this->carPtr[i];
+    delete [] this->carPtr;
+}
+
+bool CarArray::add(string id, bool isOn, bool isSer, double lon, double lat)
+{
+    if(this->cnt < this->capacity){
+        this->carPtr[cnt] = new Car(id, isOn, isSer, lon, lat);
+        this->cnt++;
+        return true;
+    }
+    else
+        return false;
+}
+
+void CarArray::print(){
+    for(int i=0; i<this->cnt; i++)
+        this->carPtr[i]->print();
+}
+
+class Passenger : public Entity
+{
+private:
+public:
+    Passenger(string id, bool isOn, bool isSer, double lon, double lat);
+    void print();
+};
+
+Passenger::Passenger(string id, bool isOn, bool isSer, double lon, double lat)
+: Entity(id, isOn, isSer, lon, lat)
+{
+}
+void Passenger::print()
+{
+    cout << this->id << ": ";
+    if(this->isOn){
+        if(this->isSer){
+            cout<<"in-service, (";
+        }
+        else{
+            cout<<"waiting, (";
+        }
+        cout << this->lon << "," << this->lat <<")";
+    }
+    else
+        cout<<"offline";
+    cout<<endl;
+
+}
 
 int main()
 {
-    Car a("5HE-313", true, true, 0, 0);
-    a.print();
+    Car c("5HE-313", true, false, 0, 0);
+    c.print();
+
+    Passenger p("B90705023", true, false, 0, 0);
+    p.print();
     return 0;
 }
